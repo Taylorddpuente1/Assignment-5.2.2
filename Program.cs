1593eea1df4d1dbc9fdc081c6231fe1b3d6a3de8
Taylo <Taylo@MARIEL>
2025-05-07 22:47:29 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿// Print the first n natural number using recursion
+
+using System.IO;
+
+namespace MyRecursion
+{
+    internal class Program
+    {
+       static void Main(string[] args )
+        {
+            Print1toN(10);
+            Console.ReadKey();
+        }
+        static int Add(int i, int j)
+        {
+            return i + j;
+        }
+        static void Print(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        static void Print1toN(int n)
+        {
+           if (n>0)
+            {
+                Print1toN(n - 1);
+                Console.WriteLine(n);
+            }
+
+            }
+        }
+    }
+
